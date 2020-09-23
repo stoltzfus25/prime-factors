2443c5f97518c954d3738c6801a20304cdfed297
Samuel Stoltzfus <samuel@superiorplastic.net>
2020-09-23 08:48:26 -0600

    Initial Commit

---
+++ PrimeFactors/PrimeFactors.h
@@ -0,0 +1,18 @@
+//
+//  PrimeFactors.h
+//  PrimeFactors
+//
+//  Created by Samuel Stoltzfus on 9/23/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrimeFactors.
+FOUNDATION_EXPORT double PrimeFactorsVersionNumber;
+
+//! Project version string for PrimeFactors.
+FOUNDATION_EXPORT const unsigned char PrimeFactorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrimeFactors/PublicHeader.h>
+
+
